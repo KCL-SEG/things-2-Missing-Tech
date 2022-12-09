c93ee9f8e034a32bca1b29dd506281f0efbfd5c6
Missing-Tech <josephgrabski31@gmail.com>
2022-12-09 13:29:00 +0000

    Improve view

--- things/views.py
+++ things/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from .forms import ThingForm
+from .models import Thing
 
 def home(request):
-    form = ThingForm()
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = ThingForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            Thing(name=form.cleaned_data['name'], description=form.cleaned_data['description'], quantity=form.cleaned_data['quantity']).save()
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = ThingForm()
     return render(request, 'home.html', {'form': form})
